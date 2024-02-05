32fd8923007be94f4702df6fe207e681b24cc902
Chris-Effah <akboateng41@gmail.com>
2024-02-05 15:00:31 -0800

    shorthand syntax

--- 0x00-ES6_basic/7-main.js
+++ 0x00-ES6_basic/7-main.js
@@ -1,5 +1,3 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
-console.log(getBudgetObject(400, 700, 900));import getBudgetObject from './7-getBudgetObject.js';
-
 console.log(getBudgetObject(400, 700, 900));
