86a8c4d0fbe564f4dc739e5bc7e7fb9dba04aaea
Chris-Effah <akboateng41@gmail.com>
2024-02-05 15:47:01 -0800

    create object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,4 @@
-export default function createEmployeesObject(departmentName, employees) {   const employeesObject = {};
-  employeesObject[departmentName] = employees;
-  return employeesObject;
-
+export default function createEmployeesObject(departmentName, employees) {
+  const employee = { [departmentName]: employees };
+  return employee;
 }
