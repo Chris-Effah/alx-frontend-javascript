c5bea3b47c8c03ca245d55d9c2d89e206fcf9da3
Chris-Effah <akboateng41@gmail.com>
2024-02-06 22:15:46 -0800

    fixed a lint error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,17 +1,9 @@
 function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
   return promise
-    .then(data => {
-      return {
-        status: 200,
-        body: 'success'
-      };
-    })
-    .catch(error => {
-      console.error('Error from the API:', error.message);
-      return new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
 export default handleResponseFromAPI;
