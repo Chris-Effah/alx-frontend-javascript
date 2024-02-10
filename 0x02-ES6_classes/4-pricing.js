af0947fd4a41402b3cfbc44bdf0fdaadc3278d64
Chris-Effah <akboateng41@gmail.com>
2024-02-09 21:51:58 -0800

    building

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -23,11 +23,10 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return '${this._amount} ${this._currency.name} (${this._currency.code})';
+    return `${this._amount} ${new Currency(this._currency.name, this._currency.code).displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
-
