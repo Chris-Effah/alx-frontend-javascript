43d5e310301a271d68ef14e53dc199f1fda5f687
Chris-Effah <akboateng41@gmail.com>
2024-02-13 07:57:46 -0800

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
+
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length));
