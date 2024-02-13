24665003d1663f076c004031162525f78c5921e9
Chris-Effah <akboateng41@gmail.com>
2024-02-13 05:08:54 -0800

    sum

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,4 @@
 export default function getStudentIdsSum(getListStudents) {
-  const sumIds = getListStudents.reduce((accumulator, student) => {
-    return accumulator + student.id;
-  }, 0);
+  const sumIds = getListStudents.reduce((accumulator, student) => accumulator + student.id, 0);
   return sumIds;
 }
