2e7ee61339df67f215c8ca232ad722e758217656
akosuaeffa <akosuaeffah24@gmail.com>
2024-02-13 13:32:42 +0000

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(getListStudents, city, newGrade
   return getListStudents
     .filter((student) => student.location === city)
     .map((student) => {
-      const grade = newGrades.find(grade => grade.studentId === student.id);
+      const grade = newGrades.find((grade) => grade.studentId === student.id);
       return { ...student, grade: (grade && grade.grade) || 'N/A' };
     });
 }
